032242b4d2578fea696f1fc360e637fc4ea4fe12
Víctor Hugo R B <victor.rojas98@gmail.com>
2024-01-03 21:15:04 -0600

    google sign-in

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { body, param } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { fieldValidator } = require('../middlewares/field-validator');
 
 const router = Router();
@@ -9,6 +9,11 @@ router.post('/login',[
     body('email', 'Email is required').isEmail(),
     body('password', 'Password is required').not().isEmpty(),
     fieldValidator
+], login );
+
+router.post('/google',[
+    body('id_token', 'id_token is required').not().isEmpty(),
+    googleSignIn
 ], login );
 
 module.exports =router;
